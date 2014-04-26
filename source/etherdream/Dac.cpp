b5cf5d8295b56316808ef994581e747114be7432
bt <bt@darwin.(none)>
2014-04-26 03:41:12 -0400

    Fix bad streaming on darwin with different value

--- source/etherdream/Dac.cpp
+++ source/etherdream/Dac.cpp
@@ -16,183 +16,183 @@
 using namespace std;
 
 Dac::Dac(string addr) :
-	address(addr),
-	port(DAC_PORT_COMMS),
-	fd(0),
-	streamer(0),
-	started(false)
+  address(addr),
+  port(DAC_PORT_COMMS),
+  fd(0),
+  streamer(0),
+  started(false)
 {
-	memset(&server, 0, sizeof server);
+  memset(&server, 0, sizeof server);
 
-	//fd = socket(AF_INET, SOCK_STREAM, 0);
-	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	
-	if (fd < 0) {
-		// TODO: ERROR
-	}
+  //fd = socket(AF_INET, SOCK_STREAM, 0);
+  fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  
+  if (fd < 0) {
+	// TODO: ERROR
+  }
 
-	//connect();
+  //connect();
 }
 
 Dac::~Dac()
 {
-	// TODO
+  // TODO
 }
 
 // TODO: This is inefficient. 
 vector<dac_point> Dac::convertPoints(Points pts)
 {
-	vector<dac_point> newPts;
+  vector<dac_point> newPts;
 
-	newPts.reserve(pts.size());
+  newPts.reserve(pts.size());
 
-	for(unsigned int i = 0; i < pts.size(); i++) {
-		Point pt = pts[i];
-		dac_point newPt;
+  for (unsigned int i = 0; i < pts.size(); i++) {
+	Point pt = pts[i];
+	dac_point newPt;
 
-		newPt.x = (int)pt.pos.x;
-		newPt.y = (int)pt.pos.y;
+	newPt.x = (int)pt.pos.x;
+	newPt.y = (int)pt.pos.y;
 
-		newPt.r = pt.color.r;
-		newPt.g = pt.color.g;
-		newPt.b = pt.color.b;
+	newPt.r = pt.color.r;
+	newPt.g = pt.color.g;
+	newPt.b = pt.color.b;
 
-		newPts.push_back(newPt);
+	newPts.push_back(newPt);
 
-		/*int k = 1;
-		while(k) {
-			// XXX: Just an experiment to waste CPU cycles
-			for(unsigned int j = 0; j < newPts.size(); j++) {
-				pt.x = newPt.x;
-				pt.y = newPt.y;
-				pt.r = newPt.r;
-				pt.g = newPt.g;
-				pt.b = newPt.b;
-			}
-			k--;
-		}*/
-	}
+	/*int k = 1;
+	while(k) {
+	  // XXX: Just an experiment to waste CPU cycles
+	  for(unsigned int j = 0; j < newPts.size(); j++) {
+		pt.x = newPt.x;
+		pt.y = newPt.y;
+		pt.r = newPt.r;
+		pt.g = newPt.g;
+		pt.b = newPt.b;
+	  }
+	  k--;
+	}*/
+  }
 
-	return newPts;
+  return newPts;
 }
 
 void Dac::connect()
 {
-	int r = 0;
-	dac_response rsp;
+  int r = 0;
+  dac_response rsp;
 
-	cout << "[dac] connecting... (" << address << ")" << endl;
+  cout << "[dac] connecting... (" << address << ")" << endl;
 
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(address.c_str());
-	server.sin_port = htons(port);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr(address.c_str());
+  server.sin_port = htons(port);
 
-    r = ::connect(fd, (sockaddr*)&server, sizeof server);
+  r = ::connect(fd, (sockaddr*)&server, sizeof server);
 
-	if (r < 0) {
-		// TODO ERROR
-		cerr << "Could not connect() to DAC!" << endl;
-		return;
-	}
+  if (r < 0) {
+	// TODO ERROR
+	cerr << "Could not connect() to DAC!" << endl;
+	return;
+  }
 
-	checkResponse('?');
+  checkResponse('?');
 }
 
 bool Dac::prepare()
 {
-	prepare_command cmd; // TODO: Could be constant
-	dac_response rsp;
+  prepare_command cmd; // TODO: Could be constant
+  dac_response rsp;
 
-	//cout << "[dac] preparing..." << endl;
+  //cout << "[dac] preparing..." << endl;
 
-	// Messy serialization
-	vector<uint8_t> cmdBuf;
-	cmdBuf = cmd.serialize();
+  // Messy serialization
+  vector<uint8_t> cmdBuf;
+  cmdBuf = cmd.serialize();
 
-	send(fd, &cmdBuf[0], cmdBuf.size(), 0);
-	return checkResponse('p');
+  send(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  return checkResponse('p');
 }
 
 bool Dac::begin()
 {
-	begin_command cmd; // TODO: Could be constant
-	dac_response rsp;
+  begin_command cmd; // TODO: Could be constant
+  dac_response rsp;
 
-	//cout << "[dac] beginning..." << endl;
+  cout << "[dac] beginning..." << endl;
 
-	// Messy serialization
-	vector<uint8_t> cmdBuf;
-	cmdBuf = cmd.serialize();
+  // Messy serialization
+  vector<uint8_t> cmdBuf;
+  cmdBuf = cmd.serialize();
 
-	send(fd, &cmdBuf[0], cmdBuf.size(), 0);
-	return checkResponse('b');
+  send(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  return checkResponse('b');
 }
 
 bool Dac::stop()
 {
-	stop_command cmd; // TODO: Could be constant
-	dac_response rsp;
+  stop_command cmd; // TODO: Could be constant
+  dac_response rsp;
 
-	cout << "[dac] stopping..." << endl;
+  cout << "[dac] stopping..." << endl;
 
-	// Messy serialization
-	vector<uint8_t> cmdBuf;
-	cmdBuf = cmd.serialize();
+  // Messy serialization
+  vector<uint8_t> cmdBuf;
+  cmdBuf = cmd.serialize();
 
-	send(fd, &cmdBuf[0], cmdBuf.size(), 0);
-	return checkResponse('s');
+  send(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  return checkResponse('s');
 }
 
 bool Dac::clear_estop()
 {
-	clear_estop_command cmd; // TODO: Could be constant
-	dac_response rsp;
+  clear_estop_command cmd; // TODO: Could be constant
+  dac_response rsp;
 
-	cout << "[dac] clearing e-stop..." << endl;
+  cout << "[dac] clearing e-stop..." << endl;
 
-	// Messy serialization
-	vector<uint8_t> cmdBuf;
-	cmdBuf = cmd.serialize();
+  // Messy serialization
+  vector<uint8_t> cmdBuf;
+  cmdBuf = cmd.serialize();
 
-	send(fd, &cmdBuf[0], cmdBuf.size(), 0);
-	return checkResponse('d');
+  send(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  return checkResponse('d');
 }
 
 // TODO: Fix messiness
 bool Dac::test_send_data(vector<dac_point> pts)
 {
-	data_command cmd;
+  data_command cmd;
 
-	cmd.set_points(pts);
-	vector<uint8_t> cmdBuf = cmd.serialize();
+  cmd.set_points(pts);
+  vector<uint8_t> cmdBuf = cmd.serialize();
 
-	send(fd, &cmdBuf[0], cmdBuf.size(), 0);
-	return checkResponse('d');
+  send(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  return checkResponse('d');
 }
 
 bool Dac::checkResponse(char command) 
 {
-	dac_response rsp;
-	vector<uint8_t> cmdBuf(22, 0);
+  dac_response rsp;
+  vector<uint8_t> cmdBuf(22, 0);
 
-	recv(fd, &cmdBuf[0], cmdBuf.size(), 0);
+  recv(fd, &cmdBuf[0], cmdBuf.size(), 0);
 
-	rsp.setFromBuffer(cmdBuf);
+  rsp.setFromBuffer(cmdBuf);
 
-	lastStatus = rsp.status;
+  lastStatus = rsp.status;
 
-	/*if(rsp.isAck()) {
-		cout << "Command acknowledged (";
-		cout << rsp.command << ")" << endl;
-	}*/
+  /*if(rsp.isAck()) {
+	  cout << "Command acknowledged (";
+	  cout << rsp.command << ")" << endl;
+  }*/
 
-	if(!rsp.isAck() || command != rsp.command) {
-		//cerr << "[!] Could not do whatever: " << command << endl;
-		//rsp.print();
-		return false;
-	}
+  if(!rsp.isAck() || command != rsp.command) {
+	//cerr << "[!] Could not do whatever: " << command << endl;
+	//rsp.print();
+	return false;
+  }
 
-	return true;
+  return true;
 }
 
 void Dac::setStreamer(Streamer* s)
@@ -202,87 +202,86 @@ void Dac::setStreamer(Streamer* s)
 
 void Dac::stream()
 {
-	// TODO: Check if connected. 
-	connect();
+  // TODO: Check if connected. 
+  connect();
 
-	// Only need to prepare if not already prepared
-	// If then can't prepare, perhaps the last run has 
-	// it confused, so try a shutdown / state refresh.
-	if(lastStatus.playback_state == 0) {
-		if(!prepare()) { 
-			cerr << "DAC: FIRST 'PREPARE' NOT ACKNOWLEDGED!" << endl;
-			if(!clear_estop()) {
-				// TODO: Raise critical exception
-				cerr << "DAC: COULD NOT CLEAR ESTOP!" << endl;
-				return;
-			}
-			stop();
-			if(!prepare()) {
-				// TODO: Raise critical exception
-				cerr << "DAC: 'PREPARE' NOT ACKNOWLEDGED!" << endl;
-				return;
-			}
-		}
+  // Only need to prepare if not already prepared
+  // If then can't prepare, perhaps the last run has 
+  // it confused, so try a shutdown / state refresh.
+  if(lastStatus.playback_state == 0) {
+	if(!prepare()) { 
+	  cerr << "DAC: FIRST 'PREPARE' NOT ACKNOWLEDGED!" << endl;
+	  if(!clear_estop()) {
+		// TODO: Raise critical exception
+		cerr << "DAC: COULD NOT CLEAR ESTOP!" << endl;
+		return;
+	  }
+	  stop();
+	  if(!prepare()) {
+		// TODO: Raise critical exception
+		cerr << "DAC: 'PREPARE' NOT ACKNOWLEDGED!" << endl;
+		return;
+	  }
+	}
+  }
+
+  // TODO: Idea [!]
+  // do a connect(), restart(), etc. functions that run through
+  // all the commands necessary for a healthy connection
+
+  started = false;
+
+  unsigned int send = 1000;
+  unsigned int guessRate = DEFAULT_POINTS_PER_SEND;
+
+  while (true) {
+	vector<dac_point> points;
+
+	// XXX: Not a perfect heuristic
+	// Send based on buffer fullness, as some fraction of 30kpps
+	guessRate = 200; // streamer->getRecommendedSendRate();
+	send = (int)(guessRate * 
+			(lastStatus.buffer_fullness/3000.0f));
+
+	if(send < 200) {
+	  //usleep(1000);
+	  //waitCount++;
+	  //continue;
 	}
 
-	// TODO: Idea [!]
-	// do a connect(), restart(), etc. functions that run through
-	// all the commands necessary for a healthy connection
+	//send = 5000;
 
-	started = false;
-
-	unsigned int send = 1000;
-	unsigned int guessRate = DEFAULT_POINTS_PER_SEND;
-
-	while(true) 
-	{
-		vector<dac_point> points;
-
-		// XXX: Not a perfect heuristic
-		// Send based on buffer fullness, as some fraction of 30kpps
-		guessRate = 200; // streamer->getRecommendedSendRate();
-		send = (int)(guessRate * 
-				(lastStatus.buffer_fullness/30000.0f));
-
-		if(send < 200) {
-			//usleep(1000);
-			//waitCount++;
-			//continue;
-		}
-
-		//send = 5000;
-
-		if(send < 1) {
-			//cout << "SEND ZERO?" << endl;
-			//cout << "buff: " << lastStatus.buffer_fullness << endl;
-			send = 1000;
-		}
-
-		// Sometimes we can flood the DAC
-		//if(started && lastStatus.buffer_fullness == 0) {
-		if(started && lastStatus.isDacFlooded()) {
-			//cout << "**FLOOD**" << endl;
-			refreshStream();
-		}
-
-		points = convertPoints(streamer->getPoints2(send));
-
-		// FIXME: These functions and their names suck.
-		if(!test_send_data(points)) {
-			//cout << "Tried to send " << points.size() << endl;
-		}
-		usleep(10000);
-
-		if(!started) {
-			started = true;
-			begin();
-		}
+	if(send < 1) {
+	  //cout << "SEND ZERO?" << endl;
+	  //cout << "buff: " << lastStatus.buffer_fullness << endl;
+	  send = 1000;
 	}
+
+	// Sometimes we can flood the DAC
+	//if(started && lastStatus.buffer_fullness == 0) {
+	if(started && lastStatus.isDacFlooded()) {
+	  cout << "**FLOOD**" << endl;
+	  refreshStream();
+	}
+
+	points = convertPoints(streamer->getPoints2(send));
+
+	// FIXME: These functions and their names suck.
+	if(!test_send_data(points)) {
+	  //cout << "Tried to send " << points.size() << endl;
+	}
+	usleep(10000);
+
+	if(!started) {
+	  started = true;
+	  begin();
+	}
+  }
 }
 
 void Dac::refreshStream()
 {
-	prepare();
-	started = false;
+  prepare();
+  started = false;
 }
 
--- source/etherdream/Dac.hpp
+++ source/etherdream/Dac.hpp
@@ -19,83 +19,79 @@ using namespace std;
  * Communicate with the DAC.
  */
 class Dac {
+  public:
+	// These are for ease of use
+	const string address;
+	const unsigned int port;
+
+	// Last status sent from DAC
+	// TODO: Temporary public
+	dac_status lastStatus;
+
+	/**
+	 * CTOR
+	 *  - string addr ip_address
+	 */
+	Dac(string addr);
+
+	/**
+	 * DTOR.
+	 */
+	~Dac();
+
+	/**
+	 * Establish TCP connection
+	 */
+	void connect();
+
+	/**
+	 * Commands 
+	 * TODO: DOC
+	 */
+	bool prepare();
+	bool begin();
+	bool stop();
+	bool clear_estop();
+
+	bool test_send_data(vector<dac_point> pts);
+
+	/**
+	 * Start the stream
+	 * TODO: These should live outside of EtherDream code.
+	 */
+	void stream();
+	void setStreamer(Streamer* s);
 
 
-	public:
-		// These are for ease of use
-		const string address;
-		const unsigned int port;
+  private:
+	// Socket file descriptor
+	int fd;
 
-		// Last status sent from DAC
-		// TODO: Temporary public
-		dac_status lastStatus;
+	// Dac address info
+	sockaddr_in server; 
 
-		/**
-		 * CTOR
-		 *  - string addr ip_address
-		 */
-		Dac(string addr);
+	// Message buffer
+	//string buffer;
+	vector<char> buffer;
 
-		/**
-		 * DTOR.
-		 */
-		~Dac();
+	// Streamer object
+	Streamer* streamer;
 
-		/**
-		 * Establish TCP connection
-		 */
-		void connect();
+	// Whether stream has started
+	bool started;
 
-		/**
-		 * Commands 
-		 * TODO: DOC
-		 */
-		bool prepare();
-		bool begin();
-		bool stop();
-		bool clear_estop();
+	vector<dac_point> convertPoints(vector<Point> pts);
 
-		bool test_send_data(vector<dac_point> pts);
+	/**
+	 * Sometimes the DAC gets flooded or buffer fills.
+	 * We need to reset the streaming.
+	 */
+	void refreshStream();
 
-		/**
-		 * Start the stream
-		 * TODO: These should live outside of EtherDream code.
-		 */
-		void stream();
-		void setStreamer(Streamer* s);
-
-
-	private:
-		// Socket file descriptor
-		int fd;
-
-		// Dac address info
-		sockaddr_in server; 
-
-		// Message buffer
-		//string buffer;
-		vector<char> buffer;
-
-		// Streamer object
-		Streamer* streamer;
-
-		// Whether stream has started
-		bool started;
-
-		vector<dac_point> convertPoints(vector<Point> pts);
-
-		/**
-		 * Sometimes the DAC gets flooded or buffer fills.
-		 * We need to reset the streaming.
-		 */
-		void refreshStream();
-
-		/**
-		 * Parse Response.
-		 */
-		bool checkResponse(char command);
-
-
+	/**
+	 * Parse Response.
+	 */
+	bool checkResponse(char command);
 
 };
 
